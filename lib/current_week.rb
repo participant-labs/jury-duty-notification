5d5c253804ad11aec15c76ef92492a0d6a0ea20e
Ben Woosley <ben.woosley@gmail.com>
2014-07-24 01:42:22 -0700

    Make current_week timezone-aware.

--- lib/current_week.rb
+++ lib/current_week.rb
@@ -1,5 +1,5 @@
 def current_week
-# we want current week to roll over on friday
-# on friday notices are posted for following monday
-3.days.from_now.beginning_of_week
+  # we want current week to roll over on friday
+  # on friday notices are posted for following monday
+  3.days.since(Date.current).beginning_of_week
 end
